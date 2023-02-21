941e61e5535764f990bdc055340c7ca180953d09
Adrian Gutierrez <adgutierrez@ikeasistencia.com>
2023-02-20 21:34:16 -0600

    modelos y consulta

--- src/controllers/multimedia.controller.js
+++ src/controllers/multimedia.controller.js
@@ -1,5 +1,30 @@
+import {sequelize} from '../database/database.js'
+import modelosInit from '../models/init-models.js'
 
+let models = modelosInit(sequelize)
 
-export const getMultimedia = (req,res) =>{
+export const getMultimedia = async (req,res) =>{
+    let response;
+    try {
+        response = await models.multimedias.findAll({
+            include:{
+                model: models.audios,
+                as: 'audios',
+                include: 'songs'
+            }
+        })
+    } catch (e) {
+        res.status(500).json({"error": e.message})
+    }
+    res.status(200).json(response);
+}
 
+export const addMultimedia = async (req,res) =>{
+    let response;
+    try {
+       
+    } catch (e) {
+        res.status(500).json({"error": e.message})
+    }
+    res.status(200).json(response);
 }
--- src/routes/multimedia.routes.js
+++ src/routes/multimedia.routes.js
@@ -1,7 +1,8 @@
 import {Router, application} from 'express';
-import {getMultimedia} from '../controllers/multimedia.controller.js';
+import {getMultimedia,addMultimedia} from '../controllers/multimedia.controller.js';
 const router = Router();
 
 router.get('/api/multimedia', getMultimedia)
+router.post('/api/agregarMultimedia', addMultimedia);
 
 export default router;
